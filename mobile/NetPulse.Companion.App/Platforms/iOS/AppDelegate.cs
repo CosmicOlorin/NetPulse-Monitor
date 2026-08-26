32ee047e30cd843988f11ea63ad750a53ce8ee10
CosmicOlorin <110067954+CosmicOlorin@users.noreply.github.com>
2026-08-27 03:27:52 +0700

    Remove unused companion platform template

--- mobile/NetPulse.Companion.App/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace NetPulse.Companion.App;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
